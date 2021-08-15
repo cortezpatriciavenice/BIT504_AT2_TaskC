ad4a5a6b5646a1726c70d87e2e4d0e6c8ba15611
cortezpatriciavenice <cortez.patriciav@outlook.com>
2021-08-15 14:42:24 +1200

    Created a new branch called "experiment_2". Made changes to the game - Updated the ball speed from 2 to 5 and switched the game colours back to its original (background as black and objects as white).

--- BIT504_AT2_TaskC/src/PongPanel.java
+++ BIT504_AT2_TaskC/src/PongPanel.java
@@ -13,9 +13,9 @@ import javax.swing.Timer;
    
   public class PongPanel extends JPanel implements ActionListener, KeyListener {
        
-       private final static Color BACKGROUND_COLOUR = Color.WHITE;
+       private final static Color BACKGROUND_COLOUR = Color.BLACK;
        private final static int TIMER_DELAY = 5;
-       private final static int BALL_MOVEMENT_SPEED = 2;
+       private final static int BALL_MOVEMENT_SPEED = 5;
        
        GameState gameState = GameState.INITIALISING;
        
@@ -68,7 +68,7 @@ import javax.swing.Timer;
           Graphics2D g2d = (Graphics2D) g.create();
               Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
               g2d.setStroke(dashed);
-              g2d.setPaint(Color.BLACK);
+              g2d.setPaint(Color.WHITE);
               g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
               g2d.dispose();
        }
